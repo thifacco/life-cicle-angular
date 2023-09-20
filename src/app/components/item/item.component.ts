9fa43b01f58fa79f8bfba7a30c37692132b9d390
Tiago Facco <thifacco@gmail.com>
2023-09-20 15:21:41 -0300

    chore: marca o item como comprado

--- src/app/components/item/item.component.ts
+++ src/app/components/item/item.component.ts
@@ -26,4 +26,12 @@ export class ItemComponent implements OnInit, OnChanges {
   editarItem() {
     this.emitItemEdit.emit(this.item);
   }
+
+  checarItem() {
+    if (this.item.comprado === true) {
+      this.item.comprado = false;
+    } else {
+      this.item.comprado = true;
+    }
+  }
 }
